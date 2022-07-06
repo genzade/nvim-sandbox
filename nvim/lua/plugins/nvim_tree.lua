91d5e2ffc36698c8271194ceb65fa346752c360c
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-07-06 11:36:24 +0200

    add some functionality around nvim_tree plugin

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -1,11 +1,11 @@
 return {
   "kyazdani42/nvim-tree.lua",
   requires = {
-    "kyazdani42/nvim-web-devicons", -- optional, for file icon
+    "kyazdani42/nvim-web-devicons",
     opt = true,
   },
-  cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-  event = "BufRead",
+  cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFileToggle" },
+  event = "BufEnter",
   config = function()
     local ok, nvim_tree = pcall(require, "nvim-tree")
 
@@ -15,6 +15,24 @@ return {
       return
     end
 
-    nvim_tree.setup()
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+
+    map("n", "<Leader>e", "<CMD>NvimTreeFindFileToggle<CR>", opts)
+
+    nvim_tree.setup(
+      {
+        view = {
+          width = 50,
+          preserve_window_proportions = true,
+          mappings = {
+            custom_only = false,
+            list = {
+              -- user mappings go here
+            },
+          },
+        },
+      }
+    )
   end,
 }
