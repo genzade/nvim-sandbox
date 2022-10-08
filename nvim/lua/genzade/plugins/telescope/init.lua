8b2ca21a120ea55e60d3c8c10db858a6ea56d532
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-10-08 16:43:45 +0100

    Add nvim-notify plugin

--- nvim/lua/genzade/plugins/telescope/init.lua
+++ nvim/lua/genzade/plugins/telescope/init.lua
@@ -30,6 +30,7 @@ local config = function()
   -- To get fzf loaded and working with telescope, you need to call
   -- load_extension, somewhere after setup function:
   telescope.load_extension("fzf")
+  telescope.load_extension("notify")
 
   local map = vim.keymap.set
 
