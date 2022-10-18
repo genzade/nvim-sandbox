10da9c44f60449b542eebe06fc3c25323dfc3e7a
Hamid Zadeh <hamidmamadzade@gmail.com>
2022-10-18 23:39:33 +0100

    Treesitter gardening

--- nvim/lua/genzade/plugins/treesitter/init.lua
+++ nvim/lua/genzade/plugins/treesitter/init.lua
@@ -4,35 +4,40 @@ local config = function()
     return
   end
 
-  ts_config.setup {
-    -- one of "all" or a list of languages
-    -- ensure_installed = "all",
-    -- TODO: switch to all post migration
-    ensure_installed = {
-      "bash",
-      "css",
-      "html",
-      "javascript",
-      "lua",
-      "markdown",
-      "ruby",
-      "scss",
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-    -- needs testing post migration
-    textobjects = {
-      select = {
+  ts_config.setup(
+    {
+      -- one of "all" or a list of languages
+      -- ensure_installed = "all",
+      -- TODO: switch to all post migration
+      ensure_installed = {
+        "bash",
+        "css",
+        "html", -- highlight not working on linux
+        "javascript",
+        "lua",
+        "markdown",
+        "ruby",
+        "scss",
+      },
+      highlight = {
         enable = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      -- needs testing post migration
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
         },
       },
-    },
-  }
+    }
+  )
 end
 
 return {
